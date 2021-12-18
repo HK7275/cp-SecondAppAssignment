077ba1022d6a8e4175e137276f3377345b1460a3
Haripatel279 <73687452+HK7275@users.noreply.github.com>
2021-12-18 12:51:32 -0500

    home

--- containers/HomeScreen.tsx
+++ containers/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text , SafeAreaView, StyleSheet} from 'react-native';
 import { auth } from '../firebase';
 
 export default function HomeScreen({ navigation }) {
@@ -15,32 +15,95 @@ export default function HomeScreen({ navigation }) {
     // Log Out Function End
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>This is the Welcome Screen!</Text>
-      <Button
-        onPress={() => navigation.navigate('Movies')}
-        title="Go to movies"
-      />
-      <Button
-        onPress={() => navigation.navigate('Weather Converter')}
-        title="Weather Converter"
-      />
-      <Button
-        onPress={() => navigation.navigate('My Movie Details')}
-        title="My Movie Details"
-      />
+    <SafeAreaView style={styles.wrapper}>
+        <View style={[styles.boxStyles2, styles.box3]}>
+        <Text style={styles.textstyle}>
+          Welcome!!!
+        </Text>
+      </View>
+      <View style={[styles.boxStyles, styles.box1]}>
+        <Button
+          onPress={() => navigation.navigate('Movies')}
+          title="Go to movies"
+        />
+      </View>
+      <View style={[styles.boxStyles, styles.box2]}>
+        <Button
+          onPress={() => navigation.navigate('Weather Converter')}
+          title="Weather Converter"
+        />
+      </View>
+      <View style={[styles.boxStyles, styles.box1]}>
+        <Button
+          onPress={() => navigation.navigate('My Movie Details')}
+          title="My Movie Details"
+        />
+      </View>
+      <View style={[styles.boxStyles, styles.box2]}>
       <Button
         onPress={() => navigation.navigate('Rohanlayout')}
-        title="Rohanlayout"
+        title="Rohan Layout"
       />
-      <Button
-        onPress={() => navigation.navigate('Image Gallery')}
-        title="Image Gallery"
-      />
-      <Button
-        onPress={logOut}
-        title="Log Out"
-      />
-    </View>
+      </View>
+      <View style={[styles.boxStyles, styles.box1]}>
+        <Button
+          onPress={() => navigation.navigate('Image Gallery')}
+          title="Image Gallery"
+        />
+      </View>
+      <View style={[styles.boxStyles, styles.box2]}>
+        <Button
+          onPress={logOut}
+          title="Log Out"
+        />
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  boxStyles: {
+    width: '25%',
+    padding: '2%' 
+  },
+
+  boxStyles2: {
+    width: '25%',
+    padding: '2%' 
+  },
+
+  box1: {
+    flex: 0.15,
+    alignSelf: 'center',
+    backgroundColor: '#99D98C',
+  },
+  box2: {
+    flex: 0.15,
+    alignSelf: 'center',
+    backgroundColor: '#52B69A'
+  },
+  box3: {
+    flex: 0.15,
+    alignSelf: 'center',
+    backgroundColor: '#386641'
+  },
+
+  textstyle:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textAlign: 'center'
+  },
+
+  defaultContainer: {
+    backgroundColor: '#21a0a0',
+    flex: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+});
